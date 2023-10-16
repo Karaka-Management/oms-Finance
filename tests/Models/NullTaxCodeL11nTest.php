11ba86462224a7c01892b2419c24362c2bb94e0b
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 22:23:57 +0000

    test fixes

---
+++ tests/Models/NullTaxCodeL11nTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\Finance\tests\Models;
+
+use Modules\Finance\Models\NullTaxCodeL11n;
+
+/**
+ * @internal
+ */
+final class NullTaxCodeL11nTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\Finance\Models\NullTaxCodeL11n
+     * @group module
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\Finance\Models\TaxCodeL11n', new NullTaxCodeL11n());
+    }
+
+    /**
+     * @covers Modules\Finance\Models\NullTaxCodeL11n
+     * @group module
+     */
+    public function testId() : void
+    {
+        $null = new NullTaxCodeL11n(2);
+        self::assertEquals(2, $null->id);
+    }
+
+    /**
+     * @covers Modules\Finance\Models\NullTaxCodeL11n
+     * @group module
+     */
+    public function testJsonSerialize() : void
+    {
+        $null = new NullTaxCodeL11n(2);
+        self::assertEquals(['id' => 2], $null);
+    }
+}
---
+++ tests/Models/NullTaxCodeTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\Finance\tests\Models;
+
+use Modules\Finance\Models\NullTaxCode;
+
+/**
+ * @internal
+ */
+final class NullTaxCodeTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\Finance\Models\NullTaxCode
+     * @group module
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\Finance\Models\TaxCode', new NullTaxCode());
+    }
+
+    /**
+     * @covers Modules\Finance\Models\NullTaxCode
+     * @group module
+     */
+    public function testId() : void
+    {
+        $null = new NullTaxCode(2);
+        self::assertEquals(2, $null->id);
+    }
+
+    /**
+     * @covers Modules\Finance\Models\NullTaxCode
+     * @group module
+     */
+    public function testJsonSerialize() : void
+    {
+        $null = new NullTaxCode(2);
+        self::assertEquals(['id' => 2], $null);
+    }
+}
