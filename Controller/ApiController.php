abb607bcccf2c703e78f9015c271bcb009e83dcf
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:10 +0200

    fix version and bugs

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Finance
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -27,7 +27,7 @@ use phpOMS\Message\ResponseAbstract;
  * Finance class.
  *
  * @package Modules\Finance
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
